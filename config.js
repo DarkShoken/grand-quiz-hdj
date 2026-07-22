7c6ad63248ddb08acb203d838521b4febd4edd0d
DarkShoken <DarkShoken@users.noreply.github.com>
2026-07-22 11:28:27 +0200

    Rend la configuration indépendante du blind test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.BLIND_TEST_CONFIG = {
+window.GRAND_QUIZ_CONFIG = {
   SUPABASE_URL: "https://rkuxwkqdgrlqhajqxdtr.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_El_xyoQgJp6FddKYUWiY9w_VwTTVLEG"
 };
